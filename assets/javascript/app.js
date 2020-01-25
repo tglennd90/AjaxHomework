0f1afefa82abff320a9e8652f0485975f35f317c
Tatum Daily <tgd9019@outlook.com>
2020-01-24 19:00:31 -0600

    Button Loop Created

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,14 +4,41 @@
     // Arrays //
     // ============================================= //
 
+        const topics = ["Space", "Moon", "America", "American Flag", "Tennessee Vols", "Tennessee Titans", "Nashville Predators"];
+
     // Objects //
     // ============================================= //
 
     // Variables //
     // ============================================= //
 
+        const contentArea = document.getElementById("contentArea");
+        const buttonArea = document.getElementById("buttonArea");
+        const gifArea = document.getElementById("gifArea");
+        const userForm = document.getElementById("userForm");
+
 // Functions //
 // ============================================= //
 
+    // Create Buttons Loop //
+    // ============================================= //
+
+        function buttons() {
+
+            for (var i = 0; i < topics.length; i++) {
+                
+                var button = document.createElement('button')
+                var buttonText = document.createTextNode(topics[i]);
+
+                button.appendChild(buttonText)
+
+                buttonArea.appendChild(button)
+                
+            }
+
+        }
+
 // Logic //
 // ============================================= //
+
+        buttons();
