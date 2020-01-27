cfe960512d2ce5eed65be539028759b9652212ea
Tatum Daily <tgd9019@outlook.com>
2020-01-27 17:32:57 -0600

    GIFs atleast on page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,10 +12,6 @@
     // Variables //
     // ============================================= //
 
-        //var queryURL; 
-            // method: "GET"
-        //var title;
-
     // ID Grabbers //
     // ============================================= //
         
@@ -40,6 +36,8 @@
                 var button = document.createElement('button')
                 var buttonText = document.createTextNode(topics[i]);
 
+                button.setAttribute("class", "btn")
+
                 button.appendChild(buttonText)
 
                 buttonArea.appendChild(button)
@@ -50,21 +48,67 @@
 
     // Capture User Input and Transform into new button //
     // ============================================= //
-        
-        // var userValue = $(".userAdd").val().trim()
 
         $(".userSubmit").on("click", function userButton() {
+
             var userValue = $(".userAdd").val().trim()
-            //console.log(userValue)
+            
             var button = document.createElement('button')
             var buttonText = document.createTextNode(userValue);
+
+            button.setAttribute("class", "btn")
 
             button.appendChild(buttonText)
 
             buttonArea.appendChild(button)
+        });
+
+    // GIPHY Call //
+    // ============================================= //
+
+        var APIKey = "vkcrV6fIMdEpaJsBPiUwvn3J4DG4zj3w";
+
+        var q;
+
+        var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + q + "&api_key=" + APIKey + "&limit=10");
+        
+        xhr.done(function(response) { 
+
+            for (var i = 0; i < response.data.length; i++) {
+                console.log(response.data[i])
+                
+                var img = document.createElement("img");
+                
+                img.src = response.data[i].images.original_still.url
+                $("img").attr("class", "test")
+                img.style = "width: 125px"
+                img.style = "height: 125px"
+
+                gifArea.prepend(img)
+
+            }
+            
+            
+        });
+
+    // Pause/Unpause GIFs //
+    // ============================================= //
+
+        $(".test").on("click", function() {
+
+            var still = response.data[i].images.original_still.url
+            var move = response.data[i].images.original.url
+
+            if (img.src === still) {
+                img.src === move
+            } else {
+                img.src === still
+            }
+
         });
   
 // Logic //
 // ============================================= //
 
         buttons();
+        
